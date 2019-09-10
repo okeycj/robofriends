bef06c57c4f7191a60bb484064ac03b6d1417d15
Azubuike Chijindu Okechukwu <30808024+okeycj@users.noreply.github.com>
2019-09-10 23:04:06 +0100

    Updates

---
+++ precache-manifest.b0946a87dc6c2af019d50504cb3b51c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc543bd499e8f2caaeb01cc04f5684a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "95d07ef6ac7ba2b8d09d",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "425f6e006f988b2fbb88",
+    "url": "/robofriends/static/css/main.85dd8f86.chunk.css"
+  },
+  {
+    "revision": "95d07ef6ac7ba2b8d09d",
+    "url": "/robofriends/static/js/2.4237d535.chunk.js"
+  },
+  {
+    "revision": "425f6e006f988b2fbb88",
+    "url": "/robofriends/static/js/main.c1d69a38.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  }
+]);
